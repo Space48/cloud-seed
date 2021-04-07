84f4cf35499a7cffa79918ab48e06a402babc522
Mohammed Nawabuddin <mohammed@space48.com>
2021-04-07 16:31:45 +0100

    Add message to enable emulator when serving locally

--- serve/index.ts
+++ serve/index.ts
@@ -9,5 +9,15 @@ export default (buildDir: string, fnPath: string, port = 5000) => {
   const fnType = manifest.type === "http" ? SignatureType.HTTP : SignatureType.CLOUDEVENT;
   const server = getServer(fn.default, fnType);
   server.listen(port);
-  console.log(`Server for ${fnPath} is now running on localhost:${port} 👍`);
+  console.log(`> Server for ${fnPath} is now running on localhost:${port} 👍`);
+  if (manifest.type !== "http") {
+    console.log(`
+      You started a GCP CloudEvent function. You should have an appropriate emulator enabled.
+      You can use :
+        \x1b[32mdocker run --rm -it google/cloud-sdk gcloud beta emulators\x1b[0m
+      to configure an emulator for your project in another terminal.
+      Example for Pub/Sub :
+        \x1b[32mdocker run --rm -it -p 8085 google/cloud-sdk gcloud beta emulators pubsub start --project=foo\x1b[0m
+    `);
+  }
 };
